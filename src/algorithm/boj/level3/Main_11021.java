91acb651d33f0011363b77e0085bfd460fcd65eb
hyungyu92 <hyungyuyoon.dev@gmail.com>
2019-11-22 17:25:40 +0900

    BOJ 3단계 문제 A+B - 7

---
+++ src/algorithm/boj/level3/Main_11021.java
@@ -0,0 +1,18 @@
+package algorithm.boj.level3;
+
+import java.util.Scanner;
+
+public class Main_11021 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int A = 0;
+		int B = 0;
+		for (int i = 1; i <= T; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println("Case #" + i +": " + (A+B));
+		}
+	
+	}
+}
