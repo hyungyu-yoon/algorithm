06f31e73c585b72d6b4099aac213732021019fa6
hyungyu92 <hyungyuyoon.dev@gmail.com>
2019-11-24 23:57:31 +0900

    BOJ 4단

---
+++ src/algorithm/boj/level4/Main_10951.java
@@ -0,0 +1,16 @@
+package algorithm.boj.level4;
+
+import java.util.Scanner;
+
+public class Main_10951 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+	
+		while(sc.hasNextInt()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+			
+		}
+	}
+}
