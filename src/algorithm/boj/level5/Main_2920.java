db17b3c04ca6c78f29937526bf8e82aeff5c3854
hyungyu92 <hyungyuyoon.dev@gmail.com>
2019-11-25 11:29:22 +0900

    BOJ 5단계 재풀이 음계

--- src/algorithm/boj/level5/Main_2920.java
+++ src/algorithm/boj/level5/Main_2920.java
@@ -2,43 +2,20 @@ package algorithm.boj.level5;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main_2920 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		boolean check = true;
-		if(n == 1) {
-			for (int i = 2; i <= 8; i++) {
-				if(i != Integer.parseInt(st.nextToken())) {
-					check = false;
-					break;
-				}
-			}
-			
-			if(check) {
-				System.out.println("ascending");
-			}else {
-				System.out.println("mixed");
-			}
-		}else if(n == 8) {
-			for (int i = 7; i >= 1; i--) {
-				if(i != Integer.parseInt(st.nextToken())) {
-					check = false;
-					break;
-				}
-			}
-			
-			if(check) {
-				System.out.println("descending");
-			}else {
-				System.out.println("mixed");
-			}
+		String s = br.readLine();
+		
+		if(s.equals("1 2 3 4 5 6 7 8")) {
+			System.out.println("ascending");
+		}else if(s.equals("8 7 6 5 4 3 2 1")) {
+			System.out.println("descending");
 		}else {
 			System.out.println("mixed");
 		}
+		
 	}
 }
