59894a4f74e082508681ffce6e9e91545a657876
hyungyu92 <hyungyuyoon.dev@gmail.com>
2019-11-29 09:27:44 +0900

    BOJ 7단계 문제 알파벳 찾기

---
+++ src/algorithm/boj/level7/Main_10809.java
@@ -0,0 +1,22 @@
+package algorithm.boj.level7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_10809 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		int[] first = new int[26];
+		Arrays.fill(first, -1);
+		for (int i = 0; i < S.length(); i++) {
+			if(first[S.charAt(i) - 'a'] == -1) {
+				first[S.charAt(i) - 'a'] = i;
+			}
+		}
+		
+		for (int i = 0; i < first.length; i++) {
+			System.out.print(first[i] + " ");
+		}
+	}
+}
