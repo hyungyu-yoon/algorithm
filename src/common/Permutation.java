14fb788449c7ff898e683f383b1a88198daefb21
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-04-07 00:35:36 +0900

    순열 v2

--- src/common/Permutation.java
+++ src/common/Permutation.java
@@ -1,33 +1,67 @@
 package common;
 
-import java.util.Arrays;
-
 public class Permutation {
 	static int cnt=0;
+	static int[] array = {1,2,3,4,5,6};
+	static boolean[] used = new boolean[array.length];
+	static int[] result = new int[array.length];
+	
 	public static void main(String[] args) {
-		int[] array = {1,2,3,4};
+		System.out.println("===== swap =====");
+		perm(0,1);
+		System.out.println("cnt="+cnt);
+		System.out.println();
+		cnt = 0;
 		
-		perm(array, 0);
+		System.out.println("===== boolean =====");
+		perm2(0,1);
 		System.out.println("cnt="+cnt);
 	}
 	
-	static void perm(int[] arr, int d) {
-		if(d==arr.length) { 
-			System.out.println(Arrays.toString(arr));
-			cnt++; 
+	// swap 활용
+	static void perm(int d,int r) {
+		if(d == r) {
+			cnt++;
+			print(array,r);
 			return;
 		}
 		
-		for(int i=d; i<arr.length; i++) {
-			swap(arr, i, d);
-			perm(arr,d+1);
-			swap(arr, i, d);
+		for(int i=d; i<array.length; i++) {
+			swap(i, d);
+			perm(d+1,r);
+			swap(i, d);
 		}
 	}
 	
-	static void swap(int[] arr, int i, int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
+	static void swap(int i, int j) {
+		int tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+	
+	// used boolean 배열 사용 
+	static void perm2(int cnt, int r) {
+		if(cnt == r) { 
+			Permutation.cnt++;
+			print(result,r);
+			return;
+		}
+		
+		for(int i=0; i<array.length; i++) {
+			if(!used[i]) { 
+				used[i] = true;
+				result[cnt] = array[i];
+				perm2(cnt+1, r); 
+				result[cnt] = 0;
+				used[i] = false;
+			}
+		}
+	}
+	
+	static void print(int[] arr, int r) {
+		for (int i = 0; i < r; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
 	}
 }
