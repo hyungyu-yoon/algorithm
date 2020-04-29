c00a9ded9fcd3e3365082cf1906a0278fc2d0739
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-04-29 23:24:37 +0900

    boj 단계 14681 풀

---
+++ src/algorithm/boj/level/level2/Main_14681.java
@@ -0,0 +1,23 @@
+package algorithm.boj.level.level2;
+
+import java.util.Scanner;
+
+public class Main_14681 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int ans = 0;
+		if(x > 0 && y > 0) {
+			ans = 1;
+		}else if(x < 0 && y > 0) {
+			ans = 2;
+		}else if(x < 0 && y < 0) {
+			ans = 3;
+		}else { 
+			ans = 4;
+		}
+		System.out.println(ans);
+	}
+}
