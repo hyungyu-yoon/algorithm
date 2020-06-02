9661fc97d805bc9ca8417d16ab6a5d80c8bc44a0
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-06-02 23:09:03 +0900

    프로그래머스 레벨1 두정수사이의합 풀이

---
+++ src/algorithm/programmers/level1/Solution_두정수사이의합.java
@@ -0,0 +1,18 @@
+package algorithm.programmers.level1;
+
+public class Solution_두정수사이의합 {
+	public long solution(int a, int b) {
+        long answer = 0;
+        
+        if(a < b) {
+        	for (int i = a; i <= b; i++) {
+				answer+=i;
+			}
+        }else {
+        	for (int i = b; i <= a; i++) {
+				answer+=i;
+			}
+        }
+        return answer;
+    }
+}
