de709b8e09f9cf38e76a617b5b35f874f8b38b8d
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-06-06 23:19:47 +0900

    프로그래머스 레벨1 서울에서김서방찾기 풀이

---
+++ src/algorithm/programmers/level1/Solution_서울에서김서방찾기.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.level1;
+
+public class Solution_서울에서김서방찾기 {
+	public String solution(String[] seoul) {
+        String answer = "";
+        
+        for (int i = 0; i < seoul.length; i++) {
+			if(seoul[i].equals("Kim")) {
+				answer = "김서방은 " + i + "에 있다";
+				break;
+			}
+		}
+        return answer;
+    }
+}
