93de7a229c7baca4c4478a113260d2b0aa89c821
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-06-08 20:13:30 +0900

    프로그래머스 레벨1 수박수박수박수박수박수 풀이

---
+++ src/algorithm/programmers/level1/Solution_수박수박수박수박수박수.java
@@ -0,0 +1,18 @@
+package algorithm.programmers.level1;
+
+public class Solution_수박수박수박수박수박수 {
+	public String solution(int n) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+			if(i%2 == 0) {
+				sb.append("수");
+			}else {
+				sb.append("박");
+			}
+		}
+        answer = sb.toString();
+        return answer;
+    }
+}
