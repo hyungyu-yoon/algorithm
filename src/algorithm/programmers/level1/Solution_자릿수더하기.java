86ec940d06cd18866859699a1139ccf77d0cca36
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-06-11 23:19:47 +0900

    프로그래머스 레벨1 자릿수더하기 풀이

---
+++ src/algorithm/programmers/level1/Solution_자릿수더하기.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.level1;
+
+public class Solution_자릿수더하기 {
+	public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0) {
+        	answer += n%10;
+        	n /= 10;
+        }
+
+
+        return answer;
+    }
+}
