cb6c2e639e3883cd17421fd4449ce9ace2fcd137
hyungyu-yoon <hyungyuyoon.dev@gmail.com>
2020-11-29 23:59:24 +0900

    프로그래머스 레벨3 가장긴팰린드롬 풀이

---
+++ src/algorithm/programmers/level3/Soultion_가장긴팰린드롬.java
@@ -0,0 +1,28 @@
+package algorithm.programmers.level3;
+
+public class Soultion_가장긴팰린드롬 {
+    public int solution(String s) {
+        for (int i = s.length(); i > 1; i--) {
+            for (int j = 0; j  + i <= s.length(); j++) {
+                boolean isPalin = true;
+                for (int k = 0, l = i  / 2; k <= l; k++) {
+                    if (s.charAt(j+k) != s.charAt(j + i - k - 1)) {
+                        isPalin = false;
+                        break;
+                    }
+                }
+
+                if (isPalin) {
+                    return i;
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Soultion_가장긴팰린드롬 s = new Soultion_가장긴팰린드롬();
+        System.out.println(s.solution("ABCCBA"));
+    }
+}
